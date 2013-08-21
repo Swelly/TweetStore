9576e40427a9127c8ba658b51269f4b916aff9f9
Wesley Carr <surfswell17@gmail.com>
2013-08-21 15:37:33 -0400

    Init with PSQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetStore::Application.config.secret_token = 'cba47dd31af88661785821925736d8098949cca86a31c911be999392ea534f120608d39eb00529177e73cbdf69688fb2e9fb854733a0449734e4152d932453bd'
